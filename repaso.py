dfdac731c61491b954e1fdcc9525826fb3210793
Carlos Gonzalez <cgonzalez@uade.edu.ar>
2026-03-18 19:49:51 -0300

    cambie la suma por multiplicacion

--- repaso.py
+++ repaso.py
@@ -2,10 +2,10 @@ print("Calculadora en Python")
 
 # numero1 = 4  
 # numero2 = 6  
-numero1 = input("Ingrese el primer número: ") 
-numero2 = input("Ingrese el segundo número: ") 
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: ")) 
 
-# Sumar los números 
-suma = numero1 + numero2 
+# Multiplicar los números 
+multiplicacion = numero1 * numero2 
 # Mostrar el resultado 
-print(f"El resultado de la suma es: {suma}") 
+print(f"El resultado de la multiplicación es: {multiplicacion}") 
